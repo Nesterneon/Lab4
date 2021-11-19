32a9f83399fa3cfd9cd5fe42cc8ad30fea886d8d
79064 <79064@MSI>
2021-11-19 12:44:49 +0300

    Добавьте файлы проекта.

---
+++ Lab4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение N");
+            int N= Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= (2 * N - 1); i += 2)
+            {
+                sum += i;
+                Console.WriteLine(sum);
+
+            }
+            Console.Write("для завершения нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
